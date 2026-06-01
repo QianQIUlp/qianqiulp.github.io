bb69b48423c01c216be4a8e416ab8498bfdecd4d
Qiu <175368140+QianQIUlp@users.noreply.github.com>
2026-06-01 20:37:29 +0800

    fix(deps): 移除站点地图外部依赖 (#46)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://qianqiulp.github.io',
   outDir: 'dist',
-  integrations: [sitemap()],
 });
